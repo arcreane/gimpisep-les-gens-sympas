50406de5933bf08347b268acdd2e2da20df71809
Th0masso <55784802+Th0masso@users.noreply.github.com>
2021-05-31 16:04:48 +0200

    refactor project + brightness

---
+++ fake-gimp/fake-gimp/Header.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+class LGS
+{
+	public:
+		cv::Mat image;
+
+		void getImageFromPath(string path) {
+			cv::Mat img = cv::imread(path);
+			image = img;
+		};
+
+		void showImage() {
+			cv::imshow("IMAGE", image);
+			cv::waitKey(0);
+		};
+
+		void dilate();
+		void resize();
+		void brightness(int val);
+};
---
+++ fake-gimp/fake-gimp/resize.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+
+void LGS::resize()
+{
+    std::cout << "LGS::resize have been executed !\n";
+}
