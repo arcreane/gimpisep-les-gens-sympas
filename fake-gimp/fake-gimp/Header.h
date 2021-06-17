697650fd53303a809545b962101aa3ffc5aab486
SabriMsekni <msaknisabri1@gmail.com>
2021-06-17 20:38:05 +0200

    Stitching update

--- fake-gimp/fake-gimp/Header.h
+++ fake-gimp/fake-gimp/Header.h
@@ -1,27 +1,35 @@
 #pragma once
 #include <iostream>
 #include <opencv2/opencv.hpp>
+
 using namespace std;
 using namespace cv;
 
 class LGS
 {
-	public:
-		cv::Mat image;
+public:
+	cv::Mat image;
+	vector<Mat> imageArray;
 
-		void getImageFromPath(string path) {
-			cv::Mat img = cv::imread(path);
-			image = img;
-		};
 
-		void showImage() {
-			cv::imshow("IMAGE", image);
-			cv::waitKey(0);
-		};
+	void getImageFromPath(string path) {
+		cv::Mat img = cv::imread(path);
+		image = img;
+	};
 
-		void resize();
-		void brightness(int val);
-		void dilateErode(int dilation_elem, int dilation_size, int dilatOrErod);
-		void CannyThreshold(int lowThreshold, int max_lowThreshold, int kernel_size);
-		void Stitching();
+	void showImage() {
+		cv::imshow("IMAGE", image);
+		cv::waitKey(0);
+	};
+
+	void addToImageArray(string path) {
+		cv::Mat img = cv::imread(path);
+		imageArray.push_back(img);
+	}
+
+	void resize();
+	void brightness(int val);
+	void dilateErode(int dilation_elem, int dilation_size, int dilatOrErod);
+	void CannyThreshold(int lowThreshold, int max_lowThreshold, int kernel_size);
+	void Stitching();
 };
--- fake-gimp/fake-gimp/Stitching.cpp
+++ fake-gimp/fake-gimp/Stitching.cpp
@@ -1,30 +1,17 @@
 #include <opencv2/opencv.hpp>
-#include "opencv2/stitching.hpp"
 #include "header.h"
 using namespace std;
 using namespace cv;
 
 void LGS::Stitching()
 {
-	vector<Mat> sourceImages;
-
 	Mat result;
-
-	// Reading the images
-	sourceImages.push_back(imread("S1.jpg"));
-	sourceImages.push_back(imread("S2.jpg"));
-	sourceImages.push_back(imread("S3.jpg"));
-	sourceImages.push_back(imread("S4.jpg"));
-	sourceImages.push_back(imread("S5.jpg"));
-
-
-
 
 
 	Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA);
 
 	// Stitch all the images present in the image array
-	Stitcher::Status status = stitcher->stitch(sourceImages, result);
+	Stitcher::Status status = stitcher->stitch(imageArray, result);
 
 	if (status != Stitcher::OK) // Check if images could not be stiched
 	{
